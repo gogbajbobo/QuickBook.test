1fad99e3baa1b59784001257df26b36cb95a9c29
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-22 14:14:45 +0300

    OAuthClient

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 require('dotenv').config()
 
-console.log(process.env.CLIENT_ID)
-console.log(process.env.CLIENT_SECRET)
-console.log(process.env.EVIRONMENT)
-console.log(process.env.REDIRECT_URI)
+const express = require('express')
+const app = express()
 
+const OAuthClient = require('intuit-oauth')
+
+app.get('/', (req, res) => res.json({ result: 'ok' }))
+
+app.get('/callback', (req, res) => {
+    res.json({ callback: true })
+})
+
+app.get('/authUri', (req,res) => {
+
+    const oauthClient = new OAuthClient({
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        environment: process.env.EVIRONMENT,
+        redirectUri: process.env.REDIRECT_URI,
+    })
+
+    const authUri = oauthClient.authorizeUri({
+        scope: [
+            OAuthClient.scopes.Accounting,
+            OAuthClient.scopes.OpenId,
+        ],
+        state: 'testState',
+    })
+
+    console.log(authUri)
+    res.redirect(authUri)
+
+})
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log(`💻 Server listening on port ${ server.address().port }`)
+})
